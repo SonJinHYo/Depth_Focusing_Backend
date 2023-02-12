157e39f730bb79cdd0ea68cce8c85d29986afa32
SonJinHYo <wlsgy95@naver.com>
2023-02-13 04:33:51 +0900

    poetry lock 이슈로 build수정

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 pip install --upgrade pip
 
-poetry lock
 poetry install
+poetry lock
 pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
